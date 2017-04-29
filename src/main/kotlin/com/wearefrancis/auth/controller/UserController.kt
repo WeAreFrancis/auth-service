c43fa5f9175c458b9aba63322bb25df175081b75
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2017-04-29 16:41:25 +0200

    feat(AS-001): add permission checking for create method

--- src/main/kotlin/com/wearefrancis/auth/controller/UserController.kt
+++ src/main/kotlin/com/wearefrancis/auth/controller/UserController.kt
@@ -4,15 +4,20 @@ import com.wearefrancis.auth.dto.ReadUserDTO
 import com.wearefrancis.auth.dto.WriteUserDTO
 import com.wearefrancis.auth.service.UserService
 import org.springframework.http.HttpStatus
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
+import java.security.Principal
 import javax.validation.Valid
 
 @RequestMapping("/users")
 @RestController
-class UserController(private val userService: UserService) {
+class UserController(
+        private val userService: UserService
+) {
     @PostMapping
+    @PreAuthorize("hasPermission(null, 'user', 'create')")
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody @Valid userDTO: WriteUserDTO): ReadUserDTO {
-        return userService.create(userDTO)
+    fun create(@RequestBody @Valid userDTO: WriteUserDTO, principal: Principal?): ReadUserDTO {
+        return userService.create(userDTO, principal != null)
     }
 }
---
+++ src/main/kotlin/com/wearefrancis/auth/security/UserPermissionEvaluator.kt
@@ -0,0 +1,24 @@
+package com.wearefrancis.auth.security
+
+import com.wearefrancis.auth.domain.User
+import org.springframework.security.access.PermissionEvaluator
+import org.springframework.security.core.Authentication
+import java.io.Serializable
+
+class UserPermissionEvaluator: PermissionEvaluator {
+    override fun hasPermission(authentication: Authentication?, targetDomainObject: Any?, permission: Any?): Boolean {
+        throw UnsupportedOperationException()
+    }
+
+    override fun hasPermission(
+            authentication: Authentication?, targetId: Serializable?, targetType: String?, permission: Any?
+    ): Boolean = when (targetType) {
+        "user" -> when(permission) {
+            "create" -> authentication == null
+                    || User.Role.ROLE_ADMIN in authentication.authorities
+                    || User.Role.ROLE_SUPER_ADMIN in authentication.authorities
+            else -> false
+        }
+        else -> false
+    }
+}
