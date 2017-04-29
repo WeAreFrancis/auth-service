f1326b074d174558bbd2c613256f6de2a3e398ae
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2017-04-29 17:36:39 +0200

    feat(AS-015): add logs in services

--- src/main/kotlin/com/wearefrancis/auth/service/UserService.kt
+++ src/main/kotlin/com/wearefrancis/auth/service/UserService.kt
@@ -7,6 +7,7 @@ import com.wearefrancis.auth.dto.mapper.ReadUserByAdminDTOMapper
 import com.wearefrancis.auth.dto.mapper.ReadUserByOwnerDTOMapper
 import com.wearefrancis.auth.exception.ObjectAlreadyExistsException
 import com.wearefrancis.auth.repository.UserRepository
+import org.slf4j.LoggerFactory
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 
@@ -17,6 +18,10 @@ class UserService(
         private val readUserByOwnerDTOMapper: ReadUserByOwnerDTOMapper,
         private val userRepository: UserRepository
 ) {
+    companion object {
+        val logger = LoggerFactory.getLogger(UserService::class.java)!!
+    }
+
     fun create(userDTO: WriteUserDTO, byAdmin: Boolean = false): ReadUserDTO {
         if (userRepository.existsByUsername(userDTO.username)) {
             throw ObjectAlreadyExistsException("Username ${userDTO.username} already used")
@@ -30,6 +35,7 @@ class UserService(
                 password = passwordEncoder.encode(userDTO.password),
                 username = userDTO.username
         )
+        logger.info("User ${user.username} created")
         return when (byAdmin) {
             true -> readUserByAdminDTOMapper.convert(userRepository.save(user))
             false -> readUserByOwnerDTOMapper.convert(userRepository.save(user))
